238b80b89bcbad081e651e85f7e8c6febf4c1b7c
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-12 16:25:02 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Ignore the preview feature in the example, we've replaced that feature with an adding likes feature instead (this is not shown in any of the templates so you'll have to add the HTML and style it as you see fit).
+
+// Deliverables
+// - A user can select the user they want to post or comment as
+// - From the create a post section, a user can:
+//     - Enter a post's image URL
+//     - Enter a post's title
+//     - Enter a post's content
+//     - Create a post and view it in the feed
+// - From the feed section, a user can:
+//     - View a post and the owner of the post
+//     - View a posts' comments and the owner of the comments
+//     - Add a comment to a post
+//   - Add a like to a post
+
+
+// Instructions
+// - Download the .zip file from https://codesandbox.io/s/js-instagram-exercise-starter-template-6vfoj
+// - Run your json-server with json-server --watch db/db.json --routes db/routes.json --static .; notice --static . this is an alternative to using Live Server, you'll be able to view your app on http://localhost:3000/
+// - Create a fetch function to get data
+// - Create render functions to show data
+// - Use event listeners and fetch to create and update data on the server
+
+// Tips
+// - In this exercise focus on practicing Javascript and fetch requests, take your time.
+// - Keep track of the currentUser in a global variable so that you have access to their id in all your functions.
+// - Think about conditional rendering when creating the preview feature.
